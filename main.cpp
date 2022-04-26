7758a4e0863bd78528eff25d0ed92efa87afa744
pjrje4 <58542025+pjrje4@users.noreply.github.com>
2022-04-26 09:15:09 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ Binary Search Tree
 #define BLACK false
 
 using namespace std;
+void balance(node*&n);
 
 void add(int num, node* &r, node* last) { // add numbers
 	if (r == NULL) {
@@ -23,20 +24,68 @@ void add(int num, node* &r, node* last) { // add numbers
 		if (r->parent != NULL) {
 			cout << r->parent->data << endl;
 		}
+		balance(r);
 	}
 	else if (num < r->data) {
 		add(num, r->left, r);
+		return;
 	}
 	else if (num > r->data) {
 		add(num, r->right, r);
+		return;
 	}
-
-	node* p = r;
+}
+void balance (node* &n) {
+	// case2 defaults
+	// get uncle (case 3)
+	node* uncle = NULL;
+	if (n->parent != NULL && n->parent->parent != NULL && n->parent->parent->left != NULL && n->parent->parent->right != NULL) { // if has uncle	
+		cout << "has uncle" << endl;
+		if (n->parent == n->parent->parent->left) { // right of gp is uncle
+			uncle = n->parent->parent->right;
+		}
+		else if (n->parent == n->parent->parent->right) { // left of gp is uncle
+			uncle = n->parent->parent->left;
+		}
+		if (uncle->rob == RED && n->parent->rob == RED) { // case 3
+			cout << "push blackness" << endl;
+			n->parent->rob = BLACK;
+			uncle->rob = BLACK;
+			n->parent->parent->rob = RED;
+			balance(n->parent->parent);
+		}
+		if (uncle->rob == BLACK) { //(case 4 and 5)
+			if (n->parent == n->parent->parent->left && n == n->parent->right) { // case 4 rotate left
+				node* temp = n->parent;
+				n->parent->parent->left = n;
+				temp->right = n->left;
+				n->left = temp;
+				//fix parents
+				n->parent = p->parent;
+				n->left->parent = n;
+				n->left->right->parent = n->left;
+			}
+			else if (n->parent == n->parent->parent->right && n == n->parent->left) // case 4 rotate right
+				node* temp = n->parent;
+				n->parent->parent->right = n;
+				temp->left = n->right;
+				n->right = temp;
+				//fix parents
+				n->parent = p->parent;
+				n->right->parent = n;
+				n->right->left->parent = n->right;
+			}
+			//call case 5 on parent? 
+		}
+	}
+	
+	// climb tree(case1)
+	node* p = n;
 	while (p->parent != NULL) {
 		p = p->parent;
 		//check double reds and black pushes
 	}
-	if (p->rob == RED && p->parent == NULL) {
+	if (p->rob == RED && p->parent == NULL) { // case1
 		p->rob = BLACK;
 	}
 	return;
